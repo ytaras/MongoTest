7a32440cabc1429041ea75abdc328049b1789861
alex-leb <alex-lebedev@hotmail.com>
2014-04-15 11:29:45 +0300

    modify show result controller for complex input

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,23 @@ class UsersController < ApplicationController
   def index
     config=YAML.load_file('lib/items_for_select.yml')
     @items = config["items"].split(" ")
+    @properties_for_select()
   end
 
   def generate_query
     properties = Hash[params[:selected_items].map{|sym| [sym.to_sym, 1]}]
     properties[:_id] = 0
-    results = MongoConnection.instance.users_collection.aggregate([{"$project" => properties}, {"$limit" => 10}])
+    number_of_results = params[:number_of_results]
+    operator = params[:operator]
+    operator_value = params[:operator_value]
+    results = MongoConnection.instance.users_collection.aggregate([
+      {"$project" => properties},
+      {"$match" => {:office =>
+        {operator => operator_value}
+        }
+      },
+      {"$limit" => number_of_results}
+    ])
     render :partial => "show_result", :locals => {:results => results}, :layout => false
   end
 
