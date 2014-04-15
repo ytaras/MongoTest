d1267eba30313aaba9f37890afa61a631057cafd
alex-leb <alex-lebedev@hotmail.com>
2014-04-15 11:54:33 +0300

    fix bug with integer/string properties

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,14 @@ class UsersController < ApplicationController
   def index
     config=YAML.load_file('lib/items_for_select.yml')
     @items = config["items"].split(" ")
-    @properties_for_select()
   end
 
   def generate_query
     properties = Hash[params[:selected_items].map{|sym| [sym.to_sym, 1]}]
     properties[:_id] = 0
-    number_of_results = params[:number_of_results]
+    number_of_results = params[:number_of_results].to_i
     operator = params[:operator]
-    operator_value = params[:operator_value]
+    operator_value = params[:operator_value].to_i
     results = MongoConnection.instance.users_collection.aggregate([
       {"$project" => properties},
       {"$match" => {:office =>
